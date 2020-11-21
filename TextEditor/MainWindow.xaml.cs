4d5985027567a24675d575638361fbec500dea95
Schoditsch Moritz <schoditsch.moritz@student.htlwrn.ac.at>
2020-11-21 17:22:17 +0100

    add New, SaveAs, Edit(Copy, Paste, etc.) & some design changes

--- TextEditor/MainWindow.xaml.cs
+++ TextEditor/MainWindow.xaml.cs
@@ -24,20 +24,55 @@ namespace TextEditor
     {
         public const int DEFAULT_FONT_SIZE = 15;
         private int zoom, currentFontSize;
+        private string currentPath;
 
         public MainWindow()
         {
             zoom = 100;
             currentFontSize = DEFAULT_FONT_SIZE;
+            currentPath = "";
+
             InitializeComponent();
         }
 
-        private void btn_Quit(object sender, RoutedEventArgs e)
+        private void updateInfo(object sender, TextChangedEventArgs e)
         {
-            Close();
+            TextBox textBox = sender as TextBox;
+            int lines = textBox.LineCount, chars = textBox.Text.Count();
+            lbl_info.Content = lines + " Line" + (lines != 1 ? "s" : "") + "  | " + chars + " Character" + (chars != 1 ? "s" : "");
         }
 
-        private void btn_Save(object sender, RoutedEventArgs e)
+        private void Zoom(object sender, MouseWheelEventArgs e)
+        {
+            if (!Keyboard.IsKeyDown(Key.LeftCtrl)) return;
+            if (e.Delta > 0) zoom += 10;
+            else zoom -= 10;
+
+            if (zoom <= 10) zoom = 10;
+            else if (zoom >= 600) zoom = 600;
+
+            ZoomInOut();
+        }
+
+        private void ResetZoom(object sender, RoutedEventArgs e)
+        {
+            zoom = 100;
+            ZoomInOut();
+        }
+
+        private void SaveFile(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (currentPath != "")
+            {
+                StreamWriter writer = new StreamWriter(currentPath);
+                writer.Write(tb_main.Text);
+                writer.Close();
+
+                tb_main.Focus();
+            } else SaveFileForce(sender, e);
+        }
+
+        private void SaveFileForce(object sender, ExecutedRoutedEventArgs e)
         {
             SaveFileDialog fileDialog = new SaveFileDialog();
             fileDialog.Filter = "txt files (*.txt)|*.txt";
@@ -47,15 +82,16 @@ namespace TextEditor
                 StreamWriter writer = new StreamWriter(fileDialog.OpenFile());
                 writer.Write(tb_main.Text);
                 writer.Close();
+                currentPath = fileDialog.FileName;
 
-                string title = "Texteditor | " + fileDialog.FileName;
+                string title = "Texteditor | " + currentPath;
                 lbl_name.Content = title;
                 Title = title;
             }
             tb_main.Focus();
         }
 
-        private void btn_Open(object sender, RoutedEventArgs e)
+        private void OpenFile(object sender, ExecutedRoutedEventArgs e)
         {
             OpenFileDialog fileDialog = new OpenFileDialog();
             fileDialog.Filter = "txt files (*.txt)|*.txt";
@@ -68,7 +104,10 @@ namespace TextEditor
                 while (!reader.EndOfStream)
                     tb_main.Text += reader.ReadLine() + "\n";
 
-                string title = "Texteditor | " + fileDialog.FileName;
+                currentPath = fileDialog.FileName;
+
+
+                string title = "Texteditor | " + currentPath;
                 lbl_name.Content = title;
                 Title = title;
                 reader.Close();
@@ -76,37 +115,24 @@ namespace TextEditor
             tb_main.Focus();
         }
 
-        private void updateInfo(object sender, TextChangedEventArgs e)
+        private void QuitProgram(object sender, ExecutedRoutedEventArgs e)
         {
-            TextBox textBox = sender as TextBox;
-            int lines = textBox.LineCount, chars = textBox.Text.Count();
-            lbl_info.Content = lines + " Line" + (lines != 1 ? "s" : "") + "  | " + chars + " Character" + (chars != 1 ? "s" : "");
+            Close();
         }
 
-        private void tb_main_zoom(object sender, MouseWheelEventArgs e)
+        private void NewFile(object sender, ExecutedRoutedEventArgs e)
         {
-            if (!Keyboard.IsKeyDown(Key.LeftCtrl)) return;
-            if (e.Delta > 0) zoom += 10;
-            else zoom -= 10;
-
-            if (zoom <= 10) zoom = 10;
-            else if (zoom >= 600) zoom = 600;
-
-            zoomInOut();
+            tb_main.Clear();
+            Title = "Texteditor";
+            lbl_name.Content = "Texteditor";
+            currentPath = "";
         }
 
-        private void reset_zoom(object sender, RoutedEventArgs e)
-        {
-            zoom = 100;
-            zoomInOut();
-        }
-
-        private void zoomInOut()
+        private void ZoomInOut()
         {
             tb_main.FontSize = currentFontSize + (zoom - 100) / 10;
             lbl_zoom_info.Content = zoom + "%";
             tb_main.Focus();
         }
-
     }
 }
